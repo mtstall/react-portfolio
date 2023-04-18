860eaf81c7c77d20c1c1019aeb246b33476400fc
mtstall <mtstallings95@gmail.com>
2023-04-18 19:43:56 -0400

    creating react app and navigation

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// We import our Welcome component from our components folder so that we can eventually return it.
+import Nav from './components/Nav';
+
+export default function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
---
+++ react-portfolio/src/components/Nav.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default function Nav() {
+  const linkStyle = { border: '1px black', padding: '5px' }; // defining the linkStyle settings
+
+  return (
+    <nav className="main-header-menu">
+      <section
+        style={{
+          display: 'flex',
+          fontFamily: 'helvetica',
+          flexDirection: 'row',
+          alignItems: 'flex-start',
+          justifyContent: 'flex-start',
+        }}
+      >
+        <div style={linkStyle}>
+          <a href="#">About Me</a>
+        </div>
+        <div style={linkStyle}>
+          <a href="#">Portfolio</a>
+        </div>
+        <div style={linkStyle}>
+          <a href="#">Contact</a>
+        </div>
+        <div style={linkStyle}>
+          <a href="#">Resume</a>
+        </div>
+      </section>
+    </nav>
+  );
+}
