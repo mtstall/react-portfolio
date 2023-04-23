bef64847e05d9f9f9bbd84de35c29917080d8b1e
mtstall <mtstallings95@gmail.com>
2023-04-23 17:41:09 -0400

    working on nav and header of app

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 // We import our Welcome component from our components folder so that we can eventually return it.
 import Nav from './components/Nav';
+import Header from './components/Header';
+//import Section from './components/Section';
 
 export default function App() {
   return (
     <div>
       <Nav />
+      <Header />
     </div>
   );
 }
---
+++ react-portfolio/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <div className="container">
+            <h1>Taylor Stallings</h1>
+        </div>
+    )
+}
