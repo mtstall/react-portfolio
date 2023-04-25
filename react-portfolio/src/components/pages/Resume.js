a8872a28881ed4e4180d3b8b2bbe7b41a7e6ec38
mtstall <mtstallings95@gmail.com>
2023-04-24 21:30:23 -0400

    working on Resume page

--- react-portfolio/src/components/pages/Resume.js
+++ react-portfolio/src/components/pages/Resume.js
@@ -1,9 +1,22 @@
 import React from "react";
+import resume from "../../public/pdfs/Stallings_Resume.pdf";
 
 export default function Resume() {
   return (
-    <div className="container">
-      My resume.
+    <div className="resume">
+      <a href={resume} download="Stallings_Resume.pdf">Click here to download my resume.</a>
+      <div class="proficiencies">
+        <h3>Proficiencies:</h3>
+        <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Node.js</li>
+        <li>React</li>
+        <li>GraphQL</li>
+        <li>MongoDB</li>
+      </ul>
+        </div>
     </div>
   );
 }
