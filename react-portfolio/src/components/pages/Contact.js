92f7b148752a9b21641b321c027a3c4523c70e69
mtstall <mtstallings95@gmail.com>
2023-04-25 20:07:04 -0400

    working on portfolio page

--- react-portfolio/src/components/pages/Contact.js
+++ react-portfolio/src/components/pages/Contact.js
@@ -3,13 +3,9 @@ import React from "react";
 export default function Contact() {
   return (
     <div>
-              <div class="form-group">
+      <div class="form-group">
         <label for="name-input">Name:</label>
-        <textarea
-          class="form-control"
-          id="name-input"
-          rows="1"
-        ></textarea>
+        <textarea class="form-control" id="name-input" rows="1"></textarea>
       </div>
       <div class="form-group">
         <label for="email-input">Email address:</label>
@@ -22,13 +18,11 @@ export default function Contact() {
       </div>
       <div class="form-group">
         <label for="message-input">Message:</label>
-        <textarea
-          class="form-control"
-          id="message-input"
-          rows="3"
-        ></textarea>
+        <textarea class="form-control" id="message-input" rows="3"></textarea>
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-primary">
+        Submit
+      </button>
     </div>
   );
 }
--- react-portfolio/src/components/pages/Portfolio.js
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -1,9 +1,174 @@
 import React from "react";
+import deployment from "../../public/images/deployment.png";
+import github from "../../public/images/github-icon.jpg";
+import workDayScheduler from "../../public/images/work-day-scheduler.png";
+import codeQuiz from "../../public/images/coding-quiz.png";
+import weatherForecast from "../../public/images/five-day-weather-forecast.png";
+import passwordGenerator from "../../public/images/password-generator.png";
+import movieMania from "../../public/images/movie-mania.png";
+import calmCalendar from "../../public/images/calm-calendar.png";
+import "../../styles/Portfolio.css";
 
 export default function Portfolio() {
   return (
-    <div className="container">
-      My portfolio.
+    <div className="applications">
+      <div class="work-day-scheduler">
+        <div class="screenshot">
+          <img
+            src={workDayScheduler}
+            alt="screenshot of deployed workday schedule app"
+          />
+        </div>
+        <div className="links">
+          <div class="deployed-app">
+            <a
+              href="https://mtstall.github.io/work-day-scheduler/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <img src={deployment} alt="deployment icon" />
+            </a>
+          </div>
+          <div class="repo">
+            <a
+              href="https://github.com/mtstall/work-day-scheduler"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <img src={github} alt="github icon for link to github repo" />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="code-quiz">
+        <div class="screenshot">
+          <img src={codeQuiz} alt="screenshot of deployed code quiz app" />
+        </div>
+        <div class="deployed-app">
+          <a
+            href="https://mtstall.github.io/coding-assessment/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img src={deployment} alt="deployment icon" />
+          </a>
+        </div>
+        <div class="repo">
+          <a
+            href="https://github.com/mtstall/coding-assessment"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img src={github} alt="github icon for link to github repo" />
+          </a>
+        </div>
+      </div>
+
+      <div class="five-day-weather-forecast">
+        <div class="screenshot">
+          <img
+            src={weatherForecast}
+            alt="screenshot of deployed weather forecast app"
+          />
+        </div>
+        <div class="deployed-app">
+          <a
+            href="https://mtstall.github.io/five-day-weather-forecast/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img src={deployment} alt="deployment icon" />
+          </a>
+        </div>
+        <div class="repo">
+          <a
+            href="https://github.com/mtstall/five-day-weather-forecast"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img src={github} alt="github icon for link to github repo" />
+          </a>
+        </div>
+      </div>
+
+      <div class="password-generator">
+        <div class="screenshot">
+          <img
+            src={passwordGenerator}
+            alt="screenshot of deployed password generator app"
+          />
+        </div>
+        <div class="deployed-app">
+          <a
+            href="https://mtstall.github.io/password-generator/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img src={deployment} alt="deployment icon" />
+          </a>
+        </div>
+        <div class="repo">
+          <a
+            href="https://github.com/mtstall/password-generator"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img src={github} alt="github icon for link to github repo" />
+          </a>
+        </div>
+      </div>
+
+      <div class="movie-mania">
+        <div class="screenshot">
+          <img src={movieMania} alt="screenshot of deployed movie mania app" />
+        </div>
+        <div class="deployed-app">
+          <a
+            href="https://mia2380.github.io/Sneak-Attack-Project-1/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img src={deployment} alt="deployment icon" />
+          </a>
+        </div>
+        <div class="repo">
+          <a
+            href="https://github.com/mia2380/Sneak-Attack-Project-1"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img src={github} alt="github icon for link to github repo" />
+          </a>
+        </div>
+      </div>
+
+      <div class="calm-calendar">
+        <div class="screenshot">
+          <img
+            src={calmCalendar}
+            alt="screenshot of deployed calm calendar app"
+          />
+        </div>
+        <div class="deployed-app">
+          <a
+            href="https://calm-calendar-app.herokuapp.com/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img src={deployment} alt="deployment icon" />
+          </a>
+        </div>
+        <div class="repo">
+          <a
+            href="https://github.com/mtstall/calm-calendar-app"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img src={github} alt="github icon for link to github repo" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
